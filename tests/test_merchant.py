a854d45c1a9c8ed34f2289b98ee0c5005e4148b0
ciurana-numo <eugene.ciurana@numo.com>
2022-07-09 13:59:04 +0000

    Merchant definition in progress 404

--- tests/test_merchant.py
+++ tests/test_merchant.py
@@ -1,13 +1,36 @@
 # vim: set fileencoding=utf-8:
 
 
+from coronado import CoronadoDuplicatesDisallowedError
+from coronado import CoronadoMalformedObjectError
+from coronado import CoronadoUnprocessableObjectError
+from coronado import TripleObject
+from coronado.auth import Auth
+from coronado.auth import Scopes
 from coronado.baseobjects import BASE_MERCHANT_CATEGORY_CODE_DICT
+from coronado.merchant import Merchant
 from coronado.merchant import MerchantCategoryCode as MCC
+# from coronado.merchant import MerchantStatus
+from coronado.merchant import SERVICE_PATH
 
 import json
+import uuid
+
+import pytest
+
+import coronado.auth as auth
 
 
 # *** globals ***
+
+
+# *** globals ***
+
+_config = auth.loadConfig()
+_auth = Auth(_config['tokenURL'], clientID = _config['clientID'], clientSecret = _config['secret'], scope = Scopes.VIEW_OFFERS)
+
+
+Merchant.initialize(_config['serviceURL'], SERVICE_PATH, _auth)
 
 
 # +++ tests +++
@@ -26,4 +49,44 @@ def test_MCC_inSnakeCaseJSON():
 
     assert _code.code == control['code']
     assert _code.description == control['description']
+
+
+
+def test_Merchant_create():
+    spec = {
+        "external_id": uuid.uuid4().hex,
+        "assumed_name": "Malwart Stores %s" % uuid.uuid4().hex,
+        "address": {
+            "complete_address": "7370 BAKER ST STE 100\nPITTSBURGH, PA 15206",
+            "line_1": "7370 BAKER ST STE 100",
+            "line_2": "",
+            "locality": "PITTSBURGH",
+            "province": "PA",
+            "postal_code": "15206",
+            "country_code": "US",
+            "latitude": 40.440624,
+            "longitude": -79.995888
+        },
+        "merchant_category_code": {
+            "code": "7998"
+        },
+        "logo_url": "https://assets.website-files.com/6287e9b993a42a7dcb001b99/6287eb0b156c574ac578dab3_Triple-Logo-Full-Color.svg"
+    }
+
+    account = Merchant.create(spec)
+    assert account
+
+    with pytest.raises(CoronadoMalformedObjectError):
+        Merchant.create(None)
+
+#     spec['external_id'] = KNOWN_ACCT_EXT_ID
+#     with pytest.raises(CoronadoDuplicatesDisallowedError):
+#         Merchant.create(spec)
+
+    spec['external_id'] = '****'
+    with pytest.raises(CoronadoUnprocessableObjectError):
+        Merchant.create(spec)
+
+
+test_Merchant_create()
 
